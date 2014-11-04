2d9fbd004d7f6b74a4c153d41c55cf03e20d64b4
Josh McMillan <josh@joshmcmillan.co.uk>
2014-11-04 17:42:12 +0000

    Add README, load Pusher key from environment variable

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Lost My Doorbell - Client
-A python script, running on a Raspberry Pi, which eats WebSocket events from Pusher and converts them into doorbell openings.
+A Python script, running on a Raspberry Pi, which eats WebSocket events from Pusher and converts them into door openings.
 
 ## Getting Started
 - `pip install https://github.com/ekulyk/PythonPusherClient/archive/master.zip` (may require `sudo`)
-- `python poll.py`
+- `PUSHER_KEY=XXX python poll.py`
+
+## Circuitry
+You're going to need a relay sitting on GPIO pin 11 of your Raspberry Pi. This will be switched for 3 seconds whenever we get an event from Pusher.
--- poll.py
+++ poll.py
@@ -1,17 +1,29 @@
 from pusherclient import Pusher
+import RPi.GPIO as GPIO
 import time
+import os
 global pusher # ugh
 
-def buzz(data):
-  print 'bzzzzzz'
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
 
-def bind_to_channel(data):
-  channel = pusher.subscribe('doorbell')
-  channel.bind('buzz', buzz)
-  
-pusher = Pusher('157ecece8ea69c91ccc6')
-pusher.connection.bind('pusher:connection_established', bind_to_channel)
-pusher.connect()
+try:
+  def buzz(data):
+    GPIO.output(11, True)
+    print 'Door open'
+    time.sleep(3)
+    GPIO.output(11, False)
+    print 'Door shut!'
 
-while True:
-  time.sleep(1)
+  def bind_to_channel(data):
+    channel = pusher.subscribe('doorbell')
+    channel.bind('buzz', buzz)
+
+  pusher = Pusher(os.environ['PUSHER_KEY'])
+  pusher.connection.bind('pusher:connection_established', bind_to_channel)
+  pusher.connect()
+
+  while True:
+    time.sleep(1)
+except:
+  GPIO.cleanup()
